8120d6143a5ba8a54ec9605ba2814ae9047dd654
Namphueng25 <namphueng.k@ku.th>
2021-10-11 16:59:43 +0700

    Update connect_connection.php

--- connect_connection.php
+++ connect_connection.php
@@ -1,1 +1,23 @@
+<?php
+    $servername = "localhost";
+    $username = "db21_090";
+    $password = "db21_090";
+    $dbname = "db21_094";
+
+    $conn = new mysqli($servername,$username,$password);
+    mysqli_set_charset($conn, "utf8");
+    if($conn->connect_error){
+        die("Connection failed:".$conn->connect_error);
+    }
+   
+
+
+    if(!$conn->select_db($dbname)){
+        die("Connection failed: " . $conn->connect_error);
+  
+    }
+  
+
+
+?>
 

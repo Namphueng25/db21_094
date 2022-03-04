482cc229b340a3d0cb03cffec30e66a4a3b07941
Namphueng <namphueng.k@ku.th>
2022-03-04 17:06:28 +0700

    Create test.php

---
+++ views/LaboratoryStaff/test.php
@@ -0,0 +1,11 @@
+<p><center><font size = "5"> Welcome to our homepage </font></center><br>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+ฺํฺ<center><font size = "5">BY  </font></center><br>
+<center><font size = "5"> น้ำผึ้ง กิติกังสดาร 62205021595555555555555559</font></center>
+</p>

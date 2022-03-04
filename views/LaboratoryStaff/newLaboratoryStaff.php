61e0bb783dfd017ba25d0c2a7b27c372ac2f0f3d
Namphueng <namphueng.k@ku.th>
2022-03-04 17:06:25 +0700

    Create newLaboratoryStaff.php

---
+++ views/LaboratoryStaff/newLaboratoryStaff.php
@@ -0,0 +1,23 @@
+<br>
+<br>
+<br>
+<center><img class="avatar" src="https://th.seaicons.com/wp-content/uploads/2015/11/document-add-icon.png" alt="avatar" width = "200"   ></center>
+<br>
+<br>
+<center><font size = "3" color = #fff>
+<form method = "GET" action = "">
+
+    <label>ID  <input type="text" name="id"/></label><br>
+<br>
+    <label>NAME  <input type="text" name="name" /></label><br>
+    <br>
+    <label>LAST NAME  <input type="text" name="lname" /></label><br>
+  
+    <br>
+    <br>
+
+    <input type="hidden" name="controller" value="LaboratoryStaff"/><br>
+    <button type="submit" name="action" value="index">Back</button>
+    <button type="submit" name="action" value="addLaboratoryStaff">Save</button>
+</form></center>
+

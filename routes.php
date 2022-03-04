3b1c3ab941ed301b6bfe5186f8ed64ff6918ddff
Namphueng <namphueng.k@ku.th>
2022-03-04 17:07:12 +0700

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,15 +1,30 @@
 <?php
-$controllers = array('pages'=>['home','error']);
+$controllers = array('pages'=>['home','error'],'LaboratoryStaff'=>['index','search','newLaboratoryStaff','addLaboratoryStaff','delete','deleteConFirm','updateForm','update'],'Laboratory'=>['index','search','newLaboratory','addLaboratory','delete','deleteConform','updateForm','update'],'CheckRT_PCR'=>['index','search','newCheckRT_PCR','addCheckRT_PCR','delete','deleteConFirm','updateForm','update']);
 
 function call($controller,$action){
     require_once("./controllers/".$controller."_controller.php");
     
     switch($controller)
     {
-        case "pages":    $controller = new PagesController();
-                        break;
+        case "pages":                   $controller = new PagesController();
+                                        break;
+        case "LaboratoryStaff":         require_once("./models/LaboratoryStaff.php");
+                                        $controller = new LaboratoryStaffController();
+                                        break;
+                                        
+        case "Laboratory":              require_once("./models/Laboratory.php");
+                                        require_once("./models/LaboratoryStaff.php");
+                                        $controller = new LaboratoryController();
+                                        break;
 
-   
+        case "CheckRT_PCR":             require_once("./models/Laboratory.php");
+                                        require_once("./models/StaffAgency.php"); 
+                                        require_once("./models/AtkTestResult.php");  
+                                        require_once("./models/CheckRT_PCR.php");  
+                                       
+                                        $controller = new CheckRT_PCRController();
+                                        break;                             
+       
     }
     $controller->{$action}();
 
